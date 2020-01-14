fa1c4d69e4f0e78b1467404dc8453b94d228256f
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-14 21:27:34 +0300

    create the main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import './App.css';
 import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
+
 
 function App() {
   return (
     <div>
       <Layout>
-        <div style={{width: 400, border: '1px solid black'}}>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     </div>
   );
