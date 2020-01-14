3f1abb9da062679f6cd8c035d8a320ad6d36b584
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-14 22:25:34 +0300

    a list of questions

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import classes from "./ActiveQuiz.module.css"
+import classes from "./ActiveQuiz.module.css";
+import AnswersList from "./AnswersList/AnswersList";
 
 
 const ActiveQuiz = (props) => (
@@ -13,10 +14,9 @@ const ActiveQuiz = (props) => (
         </p>
 
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <AnswersList
+                answers = {props.answers}
+            />
         </ul>
     </div>
 )
