5867db44d8df9ecf74ae6f2476f42c72eb59be60
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-15 23:19:00 +0300

    page creation with route and switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from "./hoc/Layout/LayoutTwo";
 import Quiz from "./containers/Quiz/Quiz";
+import {Route, Switch} from 'react-router-dom'
+import Auth from "./containers/Auth/Auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizListt from "./containers/QuizList/QuizListt";
 
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Quiz />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return(
+            <Layout>
+                <Switch>
+                    <Route path = "/auth" component={Auth}/>
+                    <Route path = "/quiz-creator" component={QuizCreator}/>
+                    <Route path = "/quiz/:id" component={Quiz}/>
+                    <Route path = "/" component={QuizListt}/>
+                </Switch>
+            </Layout>
+        )
+    }
 }
 
 export default App;
---
+++ src/containers/QuizList/QuizListt.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class QuizListt extends Component {
+    render() {
+        return(
+            <div>
+                <h1>QuizListt</h1>
+            </div>
+        )
+    }
+}
+export default QuizListt
