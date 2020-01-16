8d10f3b762711e7ef153cd6bd0c102bd1ee377ea
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-16 19:36:10 +0300

    test list page

--- src/containers/QuizList/QuizListt.js
+++ src/containers/QuizList/QuizListt.js
@@ -1,10 +1,33 @@
 import React, {Component} from "react";
+import classes from "./QuizListt.module.css"
+import {NavLink} from "react-router-dom";
 
 class QuizListt extends Component {
+
+    renderQuizes() {
+        return [1,2,3].map((quiz, index) => {
+            return (
+                <li
+                    key={index}
+                >
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return(
-            <div>
-                <h1>QuizListt</h1>
+            <div className={classes.QuizListt}>
+                <div>
+                    <h1>Список тестов</h1>
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
+
             </div>
         )
     }
